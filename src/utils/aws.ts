d8ab65b2fa422a131a9cf64f39ec49bc07e96ee2
julio <julio.sanic.gt.256@gmail.com>
2024-05-06 15:32:33 -0600

    corrigiendo env variables

--- src/utils/aws.ts
+++ src/utils/aws.ts
@@ -1,9 +1,9 @@
 import { S3Client, DeleteObjectCommand, GetObjectCommand, PutObjectCommand } from "@aws-sdk/client-s3";
 
-const bucketName = process.env.BUCKET_NAME
-const accessKeyId = process.env.AWS_ACCESS_KEY_ID
-const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY
-const region = process.env.AWS_REGION_NAME
+const bucketName = process.env.BUCKET
+const accessKeyId = process.env.KEY_ID
+const secretAccessKey = process.env.ACCESS_KEY
+const region = process.env.REGION
 
 
 if(!bucketName || !accessKeyId || !secretAccessKey || !region) {
